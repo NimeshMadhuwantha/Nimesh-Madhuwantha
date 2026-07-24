d4818768e43098780ef286d13880699fd0821b16
Nimesh Madhuwantha <janmadhuwantha@gmail.com>
2026-07-24 21:26:14 +0530

    initial update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
+  {
+    ignores: [".next/**", "node_modules/**", "out/**", "next-env.d.ts"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
